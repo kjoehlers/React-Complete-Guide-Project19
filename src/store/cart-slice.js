39217c4a793a6810eb4b9ab50ee044e986a7f064
Ken Oehlers <kjojr56@gmail.com>
2023-03-27 11:21:00 -0400

    Commit before Lesson 253

---
+++ src/store/cart-slice.js
@@ -0,0 +1,25 @@
+import { createSlice, CreateSlice } from "@reduxjs/toolkit";
+
+createSlice({
+  name: "cart",
+  initialState: { items: [], totalQuantity: 0, changed: false },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if(!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {}},
+  }
+})
